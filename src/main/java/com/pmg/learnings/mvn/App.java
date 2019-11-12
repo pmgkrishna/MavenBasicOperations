4c27ad350b000307e8e749899e2206bfa0e8d4cf
gopala krishnan <gopalakrishnan.mariyappan@freshworks.com>
2019-11-12 16:08:23 +0530

    initial commit

---
+++ src/main/java/com/pmg/learnings/mvn/App.java
@@ -0,0 +1,14 @@
+package com.pmg.learnings.mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //nothing has to change
+        System.out.println( "Hello World!" );
+    }
+}
